ceecce7817751132f64fc697d874868ed33ac315
Yashashwini11 <727822tucs258@skct.edu.in>
2023-12-13 08:02:48 +0530

    Day_2

--- Day_2/sampleapp/src/App.js
+++ Day_2/sampleapp/src/App.js
@@ -1,13 +1,18 @@
 import './assets/css/Style.css'
-import Nav from '/Nav'
+import Nav from "./Componets/Nav";
+import Footer from "./Componets/Footer";
 function App(){
     return(
+    <div>
+        <Nav/>
+        <Footer/>
         <form class="container">
-        <h3>LOGIN PAGE</h3>
-        <input type="text" className="it" name="Username" id="" placeholder="username"/>
-        <input type="password" className="it"name="password" id="" placeholder="password"/>
-        <input type="button"className="bt" value="SUBMIT"/>
-    </form>
+            <h3>LOGIN PAGE</h3>
+            <input type="text" className="it" name="Username" id="" placeholder="username"/>
+            <input type="password" className="it"name="password" id="" placeholder="password"/>
+            <input type="button"className="bt" value="SUBMIT"/>
+        </form>
+    </div>
     )
 }
 export default App
--- Day_2/sampleapp/src/Componets/Nav.js
+++ Day_2/sampleapp/src/Componets/Nav.js
@@ -1,14 +1,15 @@
-
     function Nav() {
-        return (
+    return (
+        <div>
             <nav className="navigation">
                 <ul>
                     <li><a href="/registeration">REGISTERATION</a></li>
                     <li><a href="/login">LOGIN</a></li>
                 </ul>
             </nav>
-        )
-    }
+        </div>
+    )
+}
 
 export default Nav
 
