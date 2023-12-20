501ff93e5b14a5dd745924604a9448e745b3f64c
Yashashwini11 <727822tucs258@skct.edu.in>
2023-12-20 22:08:06 +0530

    Day_8

---
+++ Day_8/dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Style from './Components/Style'
+import './Assets/CSS/style.css'
+function App() {
+  return (
+    <>
+    <Style/>
+    </>
+
+  )
+}
+
+export default App
---
+++ Day_8/dashboard/src/Components/Style.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Style() {
+  return (
+    <>
+      <div className='main hv-100 wv-100 flex-col'>
+        <div className='appbar hv-10  wv-100'>
+
+        </div>
+        <div className='content-container'>
+          <div className='leftbar'>
+            Leftbar
+          </div>
+          <div className='content'>
+            Mycontent
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Style
